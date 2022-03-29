959a1f4321d38b73ca0e68358279a0c64ce5110d
diego-008 <diegoacetun00345@gmail.com>
2022-03-29 17:27:55 -0600

    Trabajando interfaz

--- python/Window.py
+++ python/Window.py
@@ -1,13 +1,20 @@
 # import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from turtle import bgcolor, st
 
 
 class Window:
     def __init__(self, window):
         self.window = window
         self.window.title('Products Aplication')
+        Grid.rowconfigure(window, (0,1,2), weight=1)
+
+        Grid.columnconfigure(window, (0), weight=1)
         
+        bg='pink'
+        font=('Arial', '10', 'bold')
+
         # self.window.configure(background='black')
         width = 1300
         height = 820
@@ -15,23 +22,49 @@ class Window:
         y = 0
         posicion = str(width) + "x" + str(height) + "+" + str(x) + "+" + str(y)
         self.window.geometry(posicion)
-        self.window.state('zoomed')
-        Label(self.window, text="Products Aplication", font=("Arial", 20), bg="pink").pack()
+        self.window['bg'] = 'pink'
+        # self.window.state('zoomed')
+        #Asi se puede cambiar el color de fondo
+        # frameForm = Frame(self.window, width=400, height=400)
+        #primero cambiar color y despues ponerle el grid
+        # frameForm['bg'] = 'red'
+        frameForm = Frame(self.window)
+        frameForm['bg'] = 'green'
+        frameForm.grid(row=0, column=0, sticky=N)
 
-        sizeButton = int(1300/3)
-        positionXButton=0
-        positionYButton=height-40
+        # frameForm['bg'] = 'red'
+        Label(frameForm, text='Name', font=font).grid(row=0, column=0)
+
+        Label(frameForm, text='Price', font=font).grid(row=1, column=0)
+
+        Label(frameForm, text='Quantity', font=font).grid(row=2, column=0)
+
+        self.entryName = Entry(frameForm, width=30, font=font).grid(row=0, column=1, pady=10)
+        self.entryPrice = Entry(frameForm, width=30, font=font).grid(row=1, column=1)
+        self.entryQuantity = Entry(frameForm, width=30, font=font).grid(row=2, column=1, pady=10)
+        
+        #* EW PARA EXTENDER HORIZONTALMENTE
+        #* NS PARA EXTENDER VERTICALMENTE
+        #* NSEW PARA EXTENDER EN TODAS LAS DIRECCIONES
+        #* columnspan indica que se extiende en todas las columnas
+        Button(frameForm, text='Add', font=font, command=self.add_product, bg='green', fg='white').grid(row=3, column=0, sticky="we", columnspan=3)
+        
         fgButton = "white"
-        Button(self.window, text="Add Product", font=("Arial", 15), bg="green", fg=fgButton, command=self.add_product).place(x=positionXButton, y=positionYButton, width=sizeButton)
-        positionXButton+=sizeButton
+        fontButton = ('Arial', '15', 'bold')
 
-        Button(self.window, text="Delete Product", font=("Arial", 15), bg="red", fg=fgButton, command=self.delete_product).place(x=positionXButton, y=positionYButton, width=sizeButton)
-        positionXButton+=sizeButton
+        # Button(self.window, text="Delete Product", bg="red", fg=fgButton, font=fontButton).grid(row=3, column=0, sticky="we")
 
-        Button(self.window, text="Update Product", font=("Arial", 15), bg="blue", fg=fgButton, command=self.update_product).place(x=positionXButton, y=positionYButton, width=sizeButton)
-
-        #Se crea la tabla
-        self.threeViews = ttk.Treeview(self.window, columns=('Name', 'Price', 'Quantity'))
+        # Button(self.window, text="Update Product", bg="blue", fg=fgButton, font=fontButton).grid(row=3, column=1, sticky="we")
+        
+        frameButtons=Frame(self.window)
+        frameButtons['bg'] = 'green'
+        frameButtons.grid(row=2, column=0, sticky="we s")
+        frameButtons.columnconfigure((0,1), weight=1)
+        Button(frameButtons, text="Update Product", bg="blue", fg=fgButton, font=fontButton).grid(row=0, column=0, sticky="we")
+        Button(frameButtons, text="Delete Product", bg="red", fg=fgButton, font=fontButton).grid(row=0, column=1, sticky="we")
+        
+        # Se crea la tabla
+        self.threeViews = ttk.Treeview(self.window, height=30, columns=('Name', 'Price', 'Quantity'))
         #Establecemos el tamaño de las columnas y centramos la tabla
         self.threeViews.column('#0', width=80, anchor=CENTER)
         self.threeViews.column('Name', width=80, anchor=CENTER)
@@ -43,9 +76,13 @@ class Window:
         self.threeViews.heading('Name', text='Name', anchor=CENTER)
         self.threeViews.heading('Price', text='Price', anchor=CENTER)
         self.threeViews.heading('Quantity', text='Quantity', anchor=CENTER)
-        self.threeViews.insert('', END, text='1', values=('Product 1', '$100', '10'))
-        self.threeViews.pack()
-        self.threeViews.place(relx=0.5, rely=0.2, anchor=CENTER)
+        for x in range(50):
+            self.threeViews.insert('', END, text=str(x), values=('Product 1', '$100', '10'))
+
+        self.threeViews.grid(row=1, column=0, sticky="n")
+
+        # Label(self.window, text='Products', font=font).grid(row=1, column=0)
+
     def add_product(self):
         pass
     def delete_product(self):
