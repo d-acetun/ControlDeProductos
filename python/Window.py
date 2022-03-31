ec114b3a06f0dd4c616bbb8754d437ee8cbeef4a
diego-008 <diegoacetun00345@gmail.com>
2022-03-30 21:18:08 -0600

    Se hizo la conexion y algunos metodos de la base de datos

--- python/Window.py
+++ python/Window.py
@@ -1,12 +1,17 @@
 # import tkinter as tk
+import imp
+from math import prod
 from tkinter import ttk
 from tkinter import *
-
+from DataBase import DataBase
+import pymysql
 class Window:
     def __init__(self, window):
         self.window = window
         self.window.title('Products Aplication')
         self.window['bg'] = 'skyblue1'
+        self.window.protocol('WM_DELETE_WINDOW', self.close_window)
+        self.Database = DataBase()
         Grid.rowconfigure(window, (0,1,2), weight=1)
 
         Grid.columnconfigure(window, (0), weight=1)
@@ -41,8 +46,10 @@ class Window:
         self.entryName = Entry(frameForm, width=30, font=font)
         self.entryName.focus()
         self.entryName.grid(row=0, column=1, pady=10, padx=10)
-        self.entryPrice = Entry(frameForm, width=30, font=font).grid(row=1, column=1, padx=10)
-        self.entryQuantity = Entry(frameForm, width=30, font=font).grid(row=2, column=1, pady=10, padx=10)
+        self.entryPrice = Entry(frameForm, width=30, font=font)
+        self.entryPrice.grid(row=1, column=1, padx=10)
+        self.entryQuantity = Entry(frameForm, width=30, font=font)
+        self.entryQuantity.grid(row=2, column=1, pady=10, padx=10)
         
         #* EW PARA EXTENDER HORIZONTALMENTE
         #* NS PARA EXTENDER VERTICALMENTE
@@ -52,7 +59,8 @@ class Window:
         style.configure("G.TButton", foreground="green", font=font)
         ttk.Button(frameForm, text="ADD", style="G.TButton", command=self.add_product).grid(row=3, column=0, sticky="we", columnspan=3, pady=10, padx=10)
         
-        self.message = Label(frameForm, text='', fg='red').grid(row=4, column=0, sticky="we", columnspan=3, padx=10)
+        self.message = Label(frameForm, text='')
+        self.message.grid(row=4, column=0, sticky="we", columnspan=3, padx=10)
         fgButton = "white"
         fontButton = ('Arial', '15', 'bold')
 
@@ -89,16 +97,53 @@ class Window:
         self.threeViews.heading('Name', text='Name', anchor=CENTER)
         self.threeViews.heading('Price', text='Price', anchor=CENTER)
         self.threeViews.heading('Quantity', text='Quantity', anchor=CENTER)
-        for x in range(50):
-            self.threeViews.insert('', END, text=str(x), values=('Product 1', '$100', '10'))
+        
+        self.get_products()
+
+
+    def get_products(self):
+        products = self.Database.selectAllData()
+        #clean table
+        self.threeViews.delete(*self.threeViews.get_children())
+        for product in products:
+            self.threeViews.insert('', END, text=str(product[0]), values=(product[1], product[2], product[3]))
 
         self.threeViews.grid(row=1, column=0, sticky="n", pady=10, padx=10)
-
-
+        
     def add_product(self):
-        print(self.entryName.get())
+        #check if the fields are empty and if the price is a number
+        if(self.entryName.get() == "" or self.entryPrice.get() == "" or self.entryQuantity.get() == ""):
+            self.message['text'] = "Please fill all the fields"
+            self.message['fg'] = "red"
+            return
+        if self.entryQuantity.get().isdigit() == False or self.isFloat(self.entryPrice.get()) == False:
+            self.message['text'] = "Incorrectly filled fields"
+            self.message['fg'] = "orange"
+            return
+
+        self.Database.addProduct(self.entryName.get(), self.entryPrice.get(), self.entryQuantity.get())
+        print(self.entryName.get().isdigit(), self.entryPrice.get().isdigit(), self.isFloat(self.entryQuantity.get()))
+        self.entryName.delete(0, END)
+        self.entryPrice.delete(0, END)
+        self.entryQuantity.delete(0, END)
+        self.message['text'] = "Product added"
+        self.message['fg'] = "green"
+        self.get_products()
+        # self.Database.addProduct("www", 3.3, 10)
         pass
     def delete_product(self):
         pass
     def update_product(self):
         pass
+
+    def isFloat(self, value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    def close_window(self):
+        self.Database.closeConnection()
+        self.window.destroy()
+        print('Cerrando')
